17dd36e2fe18bfad0198563a839ed6f0ce0a17f7
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2026-08-16 23:48:41 +0600

    Time: 4 ms (23.44%), Space: 19.4 MB (5.21%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,15 +1,20 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        count = {}
-        for ch in t:
-            count[ch] = count.get(ch, 0) + 1
-
-        for ch in s:
-            count[ch] = count.get(ch, 0) - 1
-
-        for ch, c in count.items():
-            if c > 0:
-                return ch
+        lis = {}
+        for  i in s:
+            if i in lis:
+                lis[i]+=1
+            else:
+                lis[i] = 1
+        for  i in t:
+            if i in lis:
+                lis[i]-=1
+            else:
+                lis[i] = 1
+        for i in lis:
+            if lis[i]:
+                return i
+        
         
 
         
